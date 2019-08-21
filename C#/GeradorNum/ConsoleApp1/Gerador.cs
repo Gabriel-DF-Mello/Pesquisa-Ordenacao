0b0dc779247aecd4a553e0e4dc5cc805f4b19b31
Laboratório 25 <usrlab25@lanfran.local>
2019-08-21 09:14:57 -0300

    adicionado projeto em C#

---
+++ C#/GeradorNum/ConsoleApp1/Gerador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorNum
+{
+    static class Gerador
+    {
+        [STAThread]
+        static public void Gerar()
+        {
+            int qtd = 0;
+            string[] lines;
+            Random rng = new Random();
+            bool next = true;
+            while (next)
+            {
+                try
+                {
+                    Console.WriteLine("Insira quantos números devem ser gerados");
+                    qtd = Convert.ToInt32(Console.ReadLine());
+                    next = false;
+                    Console.Clear();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Escreva um número");
+                }
+            }
+
+            lines = new string[qtd];
+            for (int i = 0; i < qtd; i += 1)
+            {
+                int n = rng.Next(1000000);
+                lines[i] = n.ToString();
+            }
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "arquivo de texto (*.txt)|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Path.GetFullPath(dialog.FileName);
+            }
+            string path = dialog.FileName;
+            System.IO.File.WriteAllLines(path, lines);
+        }
+
+
+        [STAThread]
+        static public int[] Ler()
+        {
+            int[] nums;
+            int i = 0;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "arquivo de texto (*.txt)|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Path.GetFullPath(dialog.FileName);
+            }
+            string path = dialog.FileName;
+
+            string[] lines = File.ReadAllLines(path);
+            nums = new int[lines.Length];
+            foreach (string line in lines)
+            {
+                nums[i] = Int32.Parse(line);
+                i += 1;
+            }
+            return nums;
+        }
+    }
+}
---
+++ C#/GeradorNum/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GeradorNum;
+
+namespace GeradorNum
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            int[] v, vO;
+            v = Gerador.Ler();
+
+            vO = Selecionador.BubbleSort(v);
+            Console.ReadLine();
+
+            foreach (int ele in vO)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
