36ed6a5c4005644a29b0ad70c7488b334f569676
Gabriel Mello <dfmello.gabriel@gmail.com>
2019-10-18 14:56:55 -0300

    added new project: Word Searching

---
+++ WordSearch/WordSearch/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WordSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTexto = new System.Windows.Forms.Label();
+            this.boxTexto = new System.Windows.Forms.RichTextBox();
+            this.labelPesquisa = new System.Windows.Forms.Label();
+            this.textBoxPesquisa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelTexto
+            // 
+            this.labelTexto.AutoSize = true;
+            this.labelTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTexto.Location = new System.Drawing.Point(12, 9);
+            this.labelTexto.Name = "labelTexto";
+            this.labelTexto.Size = new System.Drawing.Size(50, 20);
+            this.labelTexto.TabIndex = 0;
+            this.labelTexto.Text = "Texto";
+            // 
+            // boxTexto
+            // 
+            this.boxTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxTexto.Location = new System.Drawing.Point(12, 32);
+            this.boxTexto.Name = "boxTexto";
+            this.boxTexto.Size = new System.Drawing.Size(675, 389);
+            this.boxTexto.TabIndex = 1;
+            this.boxTexto.Text = "";
+            // 
+            // labelPesquisa
+            // 
+            this.labelPesquisa.AutoSize = true;
+            this.labelPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPesquisa.Location = new System.Drawing.Point(12, 434);
+            this.labelPesquisa.Name = "labelPesquisa";
+            this.labelPesquisa.Size = new System.Drawing.Size(78, 20);
+            this.labelPesquisa.TabIndex = 2;
+            this.labelPesquisa.Text = "Pesquisa";
+            // 
+            // textBoxPesquisa
+            // 
+            this.textBoxPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPesquisa.Location = new System.Drawing.Point(105, 431);
+            this.textBoxPesquisa.Name = "textBoxPesquisa";
+            this.textBoxPesquisa.Size = new System.Drawing.Size(550, 30);
+            this.textBoxPesquisa.TabIndex = 3;
+            this.textBoxPesquisa.TextChanged += new System.EventHandler(this.textBoxPesquisa_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 471);
+            this.Controls.Add(this.textBoxPesquisa);
+            this.Controls.Add(this.labelPesquisa);
+            this.Controls.Add(this.boxTexto);
+            this.Controls.Add(this.labelTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTexto;
+        private System.Windows.Forms.RichTextBox boxTexto;
+        private System.Windows.Forms.Label labelPesquisa;
+        private System.Windows.Forms.TextBox textBoxPesquisa;
+    }
+}
+
---
+++ WordSearch/WordSearch/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxPesquisa_TextChanged(object sender, EventArgs e)
+        {
+            boxTexto.SelectionStart = 0;
+            boxTexto.SelectionLength = boxTexto.Text.Length;
+            boxTexto.SelectionBackColor = Color.White;
+            boxTexto.SelectionColor = Color.Black;
+
+            string search = textBoxPesquisa.Text.ToLower();
+            string text = boxTexto.Text;
+            text = text.ToLower();
+            List<int> indexes = new List<int>();
+
+            if ((text.Contains(search)) && (text.Length > 0))
+            {
+                for (int i = 0; (i != -1)&&(i < text.Length);)
+                {
+                    i = text.IndexOf(search, i);
+                    if (i == -1)
+                    {
+                        continue;
+                    }
+                    boxTexto.SelectionStart = i;
+                    boxTexto.SelectionLength = search.Length;
+                    boxTexto.SelectionColor = Color.White;
+                    boxTexto.SelectionBackColor = Color.Crimson;
+                    i += 1;
+                }
+            }
+        }
+    }
+}
